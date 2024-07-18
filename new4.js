92c4492f7d06adc9f2270c8e36324a9731af19ab
gname1297 <gname1297@gmail.com>
2024-07-18 17:51:41 +0530

    Number Guessing Game

---
+++ new4.js
@@ -0,0 +1,42 @@
+// while loop
+//2.34.54
+
+
+const min=1;
+const max=100;
+const answer=(Math.floor(Math.random()*(max-min+1))+min)
+
+
+let guess;
+let attempt=0;
+let running=true;
+
+while(running){
+guess=window.prompt(`Enter the number bewtween ${min}- ${max}`);
+guess=Number(guess);
+
+if(isNaN(guess)){
+    window.alert("Enter a valid number");
+}
+else if (guess>max || guess<min){
+    window.alert(`Enter a number between ${min}-${max} `);
+}
+
+else {
+attempt++;
+if(guess>answer){
+    window.alert("Number is TOO high");
+}
+else if(guess<answer){
+    window.alert("Number is Too low");
+}
+else {
+   window.alert(`yey, correct ${attempt} attempt`);
+running = false;
+}
+
+
+
+}
+}
+
